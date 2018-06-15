23f2979041d20d611eac0e0c6cfc312d72824c71
David Curthoys <david.curthoys@googlemail.com>
2018-06-15 13:51:21 +0100

    strange refactorings & fixed renderer to draw outer lines

--- core/src/personal/davidcurthoys/gameoflife/LifeOfGame.kt
+++ core/src/personal/davidcurthoys/gameoflife/LifeOfGame.kt
@@ -6,7 +6,7 @@ import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 
-fun countNeighbours(map: Array<out Array<out Boolean>>, x: Int, y: Int): Int {
+fun countNeighbours(world: Array<out Array<out Boolean>>, x: Int, y: Int): Int {
     return listOf(
             Pair(-1, 0),
             Pair(-1, -1),
@@ -16,13 +16,12 @@ fun countNeighbours(map: Array<out Array<out Boolean>>, x: Int, y: Int): Int {
             Pair(1, 1),
             Pair(0, -1),
             Pair(0, 1))
-            .mapNotNull { map.getOrNull(x + it.first)?.getOrNull(y + it.second) }
+            .mapNotNull { world.getOrNull(x + it.first)?.getOrNull(y + it.second) }
             .count { it }
 }
 
 fun updateCell(isAlive: Boolean, neighbourCount: Int) =
-        isAlive && neighbourCount in 2..3
-                || neighbourCount == 3
+        isAlive && neighbourCount == 2 || neighbourCount == 3
 
 fun updateMap(world: Array<out Array<out Boolean>>) =
         Array(world.size) { x ->
@@ -35,27 +34,22 @@ class LifeOfGame : ApplicationAdapter() {
     private lateinit var shapeRenderer: ShapeRenderer
     private val rows = 100
     private val columns = 100
-    private val updateFrequency = 1.0f
-    var updateTimer = 0f
     private var world = Array(columns) { Array(rows) { false } }
-    var paused = false
-
-    override fun create() {
-        shapeRenderer = ShapeRenderer()
-    }
-
+    private val updateFrequency = 1.0f
+    private var updateTimer = 0f
+    private var paused = false
+    override fun create() { shapeRenderer = ShapeRenderer() }
+    override fun dispose() { shapeRenderer.dispose() }
     override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClearColor(0.3f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-
         val spacing = 7.5f
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line)
+        shapeRenderer.setColor(0.3f, 0.3f, 0.3f, 1f)
         for (x in 0 until world.size) {
-            shapeRenderer.setColor(0.3f, 0.3f, 0.3f, 1f)
             shapeRenderer.line(x * spacing, 0f, x * spacing, world[x].size * spacing)
         }
         for (y in 0 until world[0].size) {
-            shapeRenderer.setColor(0.3f, 0.3f, 0.3f, 1f)
             shapeRenderer.line(0f, y * spacing, world.size * spacing, y * spacing)
         }
         shapeRenderer.end()
@@ -70,23 +64,16 @@ class LifeOfGame : ApplicationAdapter() {
         if (!paused) {
             updateTimer += Gdx.graphics.deltaTime
             if (updateFrequency < updateTimer) {
-                updateTimer = 0f
+                updateTimer -= updateFrequency
                 world = updateMap(world)
             }
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) { Gdx.app.exit() }
-        paused = if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) !paused else paused
+        paused = Gdx.input.isKeyJustPressed(Input.Keys.SPACE) != paused
         val pointerX = (Gdx.input.x / spacing).toInt()
         val pointerY = ((Gdx.graphics.height - Gdx.input.y - 1) / spacing).toInt()
-        if (pointerX in 0 until world.size && pointerY in 0 until world[pointerX].size) {
-            if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) { world[pointerX][pointerY] = true }
-            if (Gdx.input.isButtonPressed(Input.Buttons.RIGHT)) { world[pointerX][pointerY] = false }
+        world.getOrNull(pointerX)?.getOrNull(pointerY)?.let {
+            world[pointerX][pointerY] = Gdx.input.isButtonPressed(Input.Buttons.LEFT) || !Gdx.input.isButtonPressed(Input.Buttons.RIGHT) && it
         }
-    }
-
-    override fun dispose() {
-        shapeRenderer.dispose()
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) { Gdx.app.exit() }
     }
 }
-
-
