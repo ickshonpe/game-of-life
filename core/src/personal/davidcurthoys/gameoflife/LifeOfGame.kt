6b64c3a8d05a29844ac375ed38b8ede31ecfcc47
David Curthoys <david.curthoys@googlemail.com>
2018-06-15 12:56:28 +0100

    Some refactorings

--- core/src/personal/davidcurthoys/gameoflife/LifeOfGame.kt
+++ core/src/personal/davidcurthoys/gameoflife/LifeOfGame.kt
@@ -6,14 +6,38 @@ import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 
-class LifeOfGame : ApplicationAdapter() {
+fun countNeighbours(map: Array<out Array<out Boolean>>, x: Int, y: Int): Int {
+    return listOf(
+            Pair(-1, 0),
+            Pair(-1, -1),
+            Pair(-1, 1),
+            Pair(1, 0),
+            Pair(1, -1),
+            Pair(1, 1),
+            Pair(0, -1),
+            Pair(0, 1))
+            .mapNotNull { map.getOrNull(x + it.first)?.getOrNull(y + it.second) }
+            .count { it }
+}
 
+fun updateCell(isAlive: Boolean, neighbourCount: Int) =
+        isAlive && neighbourCount in 2..3
+                || neighbourCount == 3
+
+fun updateMap(world: Array<out Array<out Boolean>>) =
+        Array(world.size) { x ->
+            Array(world[x].size) { y ->
+                updateCell(world[x][y], countNeighbours(world, x, y))
+            }
+        }
+
+class LifeOfGame : ApplicationAdapter() {
     private lateinit var shapeRenderer: ShapeRenderer
     private val rows = 100
     private val columns = 100
     private val updateFrequency = 1.0f
     var updateTimer = 0f
-    private var map = Array(columns) { Array(rows) { false } }
+    private var world = Array(columns) { Array(rows) { false } }
     var paused = false
 
     override fun create() {
@@ -26,55 +50,37 @@ class LifeOfGame : ApplicationAdapter() {
 
         val spacing = 7.5f
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line)
-        for (x in 0 until columns) {
+        for (x in 0 until world.size) {
             shapeRenderer.setColor(0.3f, 0.3f, 0.3f, 1f)
-            shapeRenderer.line(x * spacing, 0f, x * spacing, rows * spacing)
+            shapeRenderer.line(x * spacing, 0f, x * spacing, world[x].size * spacing)
         }
-        for (y in 0 until rows) {
+        for (y in 0 until world[0].size) {
             shapeRenderer.setColor(0.3f, 0.3f, 0.3f, 1f)
-            shapeRenderer.line(0f, y * spacing, columns * spacing, y * spacing)
+            shapeRenderer.line(0f, y * spacing, world.size * spacing, y * spacing)
         }
         shapeRenderer.end()
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled)
         shapeRenderer.setColor(1.0f, 1.0f, 1.0f, 1f)
-        for (x in 0 until columns) {
-            for (y in 0 until rows) {
-                if (map[x][y]) {
-                    shapeRenderer.rect(x * spacing, y * spacing, 1f * spacing, 1f * spacing)
-                }
+        world.forEachIndexed { x, r ->
+            r.forEachIndexed { y, isAlive ->
+                    if (isAlive) { shapeRenderer.rect(x * spacing, y * spacing, spacing, spacing) }
             }
         }
-
         shapeRenderer.end()
         if (!paused) {
             updateTimer += Gdx.graphics.deltaTime
-            if (updateTimer > updateFrequency) {
+            if (updateFrequency < updateTimer) {
                 updateTimer = 0f
-                map = updateMap(map)
+                world = updateMap(world)
             }
         }
-
-        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
-            Gdx.app.exit()
-        }
-
-        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
-            paused = !paused
-        }
-
-
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) { Gdx.app.exit() }
+        paused = if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) !paused else paused
         val pointerX = (Gdx.input.x / spacing).toInt()
         val pointerY = ((Gdx.graphics.height - Gdx.input.y - 1) / spacing).toInt()
-        if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
-            if (pointerX in 0 until map.size && pointerY in 0 until map[pointerX].size) {
-                map[pointerX][pointerY] = true
-            }
-        }
-
-        if (Gdx.input.isButtonPressed(Input.Buttons.RIGHT)) {
-            if (pointerX in 0 until map.size && pointerY in 0 until map[pointerX].size) {
-                map[pointerX][pointerY] = false
-            }
+        if (pointerX in 0 until world.size && pointerY in 0 until world[pointerX].size) {
+            if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) { world[pointerX][pointerY] = true }
+            if (Gdx.input.isButtonPressed(Input.Buttons.RIGHT)) { world[pointerX][pointerY] = false }
         }
     }
 
@@ -82,32 +88,5 @@ class LifeOfGame : ApplicationAdapter() {
         shapeRenderer.dispose()
     }
 }
-
-fun countNeighbours(map: Array<out Array<out Boolean>>, x: Int, y: Int): Int {
-    return listOf(
-            Pair(x - 1, y),
-            Pair(x - 1, y - 1),
-            Pair(x - 1, y + 1),
-            Pair(x + 1, y),
-            Pair(x + 1, y - 1),
-            Pair(x + 1, y + 1),
-            Pair(x, y - 1),
-            Pair(x, y + 1))
-            .mapNotNull { (x, y) -> map.getOrNull(x)?.getOrNull(y) }
-            .count { it }
-}
-
-fun updateCell(isAlive: Boolean, neighbourCount: Int) =
-        isAlive && neighbourCount in 2..3
-        || neighbourCount == 3
-
-fun updateMap(map: Array<out Array<out Boolean>>) =
-        Array(map.size) { x ->
-            Array(map[x].size) { y ->
-                updateCell(map[x][y], countNeighbours(map, x, y))
-            }
-        }
-
-
 
 
